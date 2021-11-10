46969a8f71f66132c7c296c3fa3d3788c42be476
김기휘 <kimgihwi@gmail.com>
2021-11-10 21:44:28 +0900

    Update 1874.py
    2021/11/10

--- 백준/silver/1874.py
+++ 백준/silver/1874.py
@@ -5,10 +5,37 @@ seq = []
 for i in range(n):
     seq.append(int(input()))
 
-stack = [0]
-
-num = list(range(1, n+1))
+seq.reverse()
+num = [x for x in range(1, n+1)]
 num.reverse()
+stack = []
 
-answer = []
+answer=[]
+yes = True
+
+while len(seq)>0:
+    ans_len = len(answer)
+    if len(stack)==0:
+        stack.append(num.pop())
+        answer.append('+')
+    if stack[-1] == seq[-1]:
+        stack.pop()
+        seq.pop()
+        answer.append('-')
+    else:
+        if len(num) == 0:
+            print('NO')
+            yes = False
+            break
+        stack.append(num.pop())
+        answer.append('+')
+
+    if ans_len == len(answer):
+        print('NO')
+        yes = False
+        break
+
+if yes:
+    for i in answer:
+        print(i)
 
